52bfb33296ce613324acb532ddc8e494d9bc7f57
adamwh94 <128773722+adamwh94@users.noreply.github.com>
2023-03-28 10:26:32 +1100

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -4,13 +4,6 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-my_data_rows = my_cur.fetchall()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_rows)
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -49,3 +42,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 #streamlit.text(fruityvice_response.json())
+
+streamlit.stop()
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+my_data_rows = my_cur.fetchall()
+streamlit.text("The fruit load list contains:")
+streamlit.text(my_data_rows)
